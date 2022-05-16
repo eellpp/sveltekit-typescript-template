322e4556d0ae4d6f63b4f0020d3735abeb275799
eellpp <6888621+eellpp@users.noreply.github.com>
2022-05-17 00:14:56 +0800

    initial version

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let message:String = "Demo of using Typescript"
+</script>
+
+
+<p>{message}</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
